23b41b657fdc6c7a841b190752d185a1d704aa0b
EricH <AvarioDeveloper@gmail.com>
2020-10-02 13:00:30 -0600

    Hack it in

---
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableRNHeadlessJs.java
@@ -0,0 +1,24 @@
+package com.iterable.iterableapi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.facebook.react.HeadlessJsTaskService;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+import javax.annotation.Nullable;
+
+public class IterableRNHeadlessJs extends HeadlessJsTaskService {
+    @Override
+    protected @Nullable HeadlessJsTaskConfig getTaskConfig(Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            return new HeadlessJsTaskConfig(
+                    "IterableAndroidHeadlessJs",
+                    Arguments.fromBundle(extras),
+                    60000, // timeout for the task
+                    true // optional: defines whether or not  the task is allowed in foreground. Default is false
+            );
+        }
+        return null;
+    }
+}
